65124644cf2496181d56232d7869d72075b820ee
Sebastian Jensen <apps@tsjdev-apps.de>
2021-09-30 22:03:56 +0200

    Add sample project containing several extension methods.

---
+++ XFExtensionMethods/XFExtensionMethods/XFExtensionMethods/Common/CollectionExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace XFExtensionMethods.Common
+{
+    public static class CollectionExtensions
+    {
+        public static bool HasItems<T>(this IEnumerable<T> collection)
+        {
+            return collection != null && collection.Any();
+        }
+
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> collection)
+        {
+            if (collection == null)
+                return new ObservableCollection<T>();
+
+            return new ObservableCollection<T>(collection);
+        }
+
+        public static void AddRange<T>(this ObservableCollection<T> targetCollection, IEnumerable<T> sourceCollection)
+        {
+            if (targetCollection == null)
+                throw new ArgumentNullException(nameof(targetCollection));
+
+            if (sourceCollection == null)
+                throw new ArgumentNullException(nameof(sourceCollection));
+
+            foreach (var item in sourceCollection)
+                targetCollection.Add(item);
+        }
+
+        public static int GetIndex<T>(this IEnumerable<T> collection, T value)
+        {
+            if (collection is IList<T> list)
+                return list.IndexOf(value);
+
+            var index = 0;
+            foreach (var item in collection)
+            {
+                if (item.Equals(value))
+                    return index;
+
+                ++index;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XFExtensionMethods/XFExtensionMethods/XFExtensionMethods/Common/ObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace XFExtensionMethods.Common
+{
+    public static class ObjectExtensions
+    {
+        public static string AsString(this object input)
+        {
+            return input == null ? string.Empty : input.ToString();
+        }
+
+        public static void WriteLine(this object input)
+        {
+            Debug.WriteLine(input.AsString());
+        }
+    }
+}
